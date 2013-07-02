adb6c514fe14fab2b1fc93f79beae76470975446
Adrian17 <adrian.wielgosik@gmail.com>
2013-07-02 09:05:02 +0200

    Simple optimization

--- CONSTRUCTS/bullets/bullet.cpp
+++ CONSTRUCTS/bullets/bullet.cpp
@@ -35,7 +35,7 @@ void Bullet::Move()
 
 bool Bullet::CheckBounds()
 {
-    if(y>SCREEN_HEIGHT+100|| y<-200 || x<-100 || x>SCREEN_WIDTH+80) return 1;
+    if(y>SCREEN_HEIGHT+50|| y<-200 || x<-80 || x>SCREEN_WIDTH+80) return 1;
     if(lifetime!=0 && time>lifetime) return 1;
     return 0;
 };
@@ -55,10 +55,11 @@ void Bullet::Draw()
     loc.x=x-32/2; loc.y=y-32/2;
     SDL_RenderCopyEx(ren, bulletSheet, &source, &loc, angle, NULL, SDL_FLIP_NONE);
 
-    dx1=(hitbox.w/2)*cos((angle)*(M_PI/180))-(hitbox.h/2)*sin((angle)*(M_PI/180));
-    dy1=(hitbox.w/2)*sin((angle)*(M_PI/180))+(hitbox.h/2)*cos((angle)*(M_PI/180));
-    dx2=(hitbox.w/2)*cos((angle)*(M_PI/180))+(hitbox.h/2)*sin((angle)*(M_PI/180));
-    dy2=(hitbox.w/2)*sin((angle)*(M_PI/180))-(hitbox.h/2)*cos((angle)*(M_PI/180));
+    double t_cos=cos(angle*(M_PI/180)); double t_sin=sin(angle*(M_PI/180));
+    dx1=(hitbox.w/2)*t_cos-(hitbox.h/2)*t_sin;
+    dy1=(hitbox.w/2)*t_sin+(hitbox.h/2)*t_cos;
+    dx2=(hitbox.w/2)*t_cos+(hitbox.h/2)*t_sin;
+    dy2=(hitbox.w/2)*t_sin-(hitbox.h/2)*t_cos;
     X1=x+dx1; Y1=y+dy1; X3=x-dx1; Y3=y-dy1;
     X2=x+dx2; Y2=y+dy2; X4=x-dx2; Y4=y-dy2;
     //SDL_RenderDrawLine(ren, X1, Y1, X2, Y2);
